da280f25e2367d358ef3cb2154b9964061e74fd8
Luis Antonio Palacios <antoniopalacios039@gmail.com>
2024-07-22 12:01:07 -0600

    Actualizacion con el avance de ventas + plantilla de pagos

--- temp_pagoscpt.py
+++ temp_pagoscpt.py
@@ -16,6 +16,8 @@ def append_row(df, row):
 # Function to create initial DataFrame
 def create_initial_dataframe():
     return pd.DataFrame(columns=['Proveedor', 'Monto', 'Facturas'])  # Update column names as needed
+def initial_dataframe():
+    return pd.DataFrame(columns=['Proveedor', 'Correo', 'Clave', 'RFC', 'Banco', 'Tipo','Persona_tipo','Id_banco'])
 
 st.set_page_config(layout="wide")
 
@@ -52,13 +54,27 @@ def main():
         prov = pd.read_csv('https://raw.githubusercontent.com/Geratano/Farmiral/main/proveedores_cuentas.csv', dtype={'Numero':str}, encoding='latin-1')
         return prov
     prov = provcuentas()
-    prov.columns = ['Proveedor', 'Correo', 'Clabe', 'RFC', 'Banco', 'Tipo']
+      # agregamos la columna id_banco la cual contiene los primeros 3 digitos de la cuenta + 0 al principio
+    try:
+        prov['Id_banco'] = '0'+ prov['Numero'].str[:3]
+    except KeyError: # si se recarga la página, las columnas del dataframe cambian, si eso sucede se ejecuta la siguiente linea con el nombre de la columna correspondiente a la nueva info
+        prov['Id_banco'] = '0'+ prov['Clave'].str[:3]
+
+    prov.columns = ['Proveedor', 'Correo', 'Clave', 'RFC', 'Banco', 'Tipo','Persona_tipo','Id_banco']
     for i in range(len(prov['Proveedor'])):
-        prov.loc[i,'Clabe'] = str(prov.loc[i,'Clabe'])
-    prov['Clabe'] = prov['Clabe'].astype(str)
+        prov.loc[i,'Clave'] = str(prov.loc[i,'Clave'])
+    prov['Clave'] = prov['Clave'].astype(str)
+    prov['RFC'] = prov['RFC'].astype(str)
+    
+  
+   
+
     # Create or retrieve DataFrame from session_state
     if 'data' not in st.session_state:
         st.session_state.data = create_initial_dataframe()
+
+    if 'filtered_data' not in st.session_state:
+        st.session_state.filtered_data = initial_dataframe()
 
     #st.write('Current DataFrame:', st.session_state.data)
     
@@ -72,11 +88,18 @@ def main():
     if st.button('Append Row'):
         # Construct a row from user input
         new_row = {'Proveedor': provedores_selec, 'Monto': cantidades_pagar, 'Facturas': fact, 'tc': tc, 'USD': cantidades_pagar/tc }
-        
+
         # Append the new row to the DataFrame
         st.session_state.data = append_row(st.session_state.data, new_row)
+        # filtro los datos por selección    
+        filtered_prov = prov[prov['Proveedor'] == provedores_selec]
+        # Convertir la fila filtrada en un DataFrame
+        if not filtered_prov.empty:
+            st.session_state.filtered_data = append_row(st.session_state.filtered_data, filtered_prov.iloc[0])
+
         
         st.success('Row appended successfully!')
+        
 
     st.write('Validacion:', st.session_state.data)
     monto_pago = st.session_state.data['Monto'].sum()
@@ -99,43 +122,79 @@ def main():
             df_temp = df_temp.merge(prov, on='Proveedor', how='left')
             #st.write(df_temp)
         except ValueError:
-            st.warning('Carga una base')
+            st.warning('Carga una base ')
+        except KeyError:
+            st.error('⚠️BASE NO COMPATIBLE, CARGA UNA BASE CORRECTA.⚠️')
+
     ref_num = st.text_input('Escribe la referencia numerica')
     ref_str = st.text_input('Escribe la referencia de pagos')
 
     if st.checkbox('Plantilla Banregio'):
-        col_names_banregio = ['Secuencia', 'Tipo', 'Cuenta_Destino', 'Importe', 'IVA', 'Descripcion', 'Ref_Numerica', 'Referencia']
-        #n=len(st.session_state.data['Proveedor'])
-        n=len(df_temp['Proveedor'])
-        sec = list(range(n))
-        tipo = ["s" for i in range(n)]
-        iva = [0]*n
-        ref = [ref_num for i in range(n)]
-        referencia = [ref_str for i in range(n)]
-        col_names_banregio = ['Secuencia', 'Tipo', 'Cuenta_Destino', 'Importe', 'IVA', 'Descripcion', 'Ref_Numerica', 'Referencia']
-        data = list(zip(sec, tipo, df_temp['Clabe'], df_temp['Monto'], iva, df_temp['Facturas'], ref, referencia))
-        df_banregio = pd.DataFrame(data, columns=col_names_banregio)
-        df_banregio['Cuenta_Destino'] = df_banregio['Cuenta_Destino'].astype(str)
-        st.write(df_banregio)     
-        def to_excel(df):
-            output = BytesIO()
-            with pd.ExcelWriter(output, engine='openpyxl') as writer:
-                df.to_excel(writer, index=False, sheet_name='Sheet1')
-            processed_data = output.getvalue()
-            return processed_data
-        def get_download_link(data, filename, text):
-            b64 = base64.b64encode(data).decode()
-            return f'<a href="data:application/octet-stream;base64,{b64}" download="{filename}">{text}</a>'
-        excel_data = to_excel(df_banregio)
-        st.markdown(get_download_link(excel_data, 'p_banregio.xlsx', 'Descargar plantilla Banregio'), unsafe_allow_html=True)
-        #def download_link(object_to_download, download_filename, download_link_text):
-        #    if isinstance(object_to_download, pd.DataFrame):
-        #        object_to_download = object_to_download.to_csv(index=False, quoting=csv.QUOTE_NONNUMERIC)
-        #    
-        #    b64 = base64.b64encode(object_to_download.encode()).decode()
-        #    return f'<a href="data:file/txt;base64,{b64}" download="{download_filename}">{download_link_text}</a>'
+        try:
+            col_names_banregio = ['Secuencia', 'Tipo', 'Cuenta_Destino', 'Importe', 'IVA', 'Descripcion', 'Ref_Numerica', 'Referencia']
+            #n=len(st.session_state.data['Proveedor'])  
+            n=len(df_temp['Proveedor'])
+            sec = list(range(n))
+            tipo = ["s" for i in range(n)]
+            iva = [0]*n
+            ref = [ref_num for i in range(n)]
+            referencia = [ref_str for i in range(n)]
+            col_names_banregio = ['Secuencia', 'Tipo', 'Cuenta_Destino', 'Importe', 'IVA', 'Descripcion', 'Ref_Numerica', 'Referencia']
+            data = list(zip(sec, tipo, df_temp['Clave'], df_temp['Monto'], iva, df_temp['Facturas'], ref, referencia))
+            df_banregio = pd.DataFrame(data, columns=col_names_banregio)
+            df_banregio['Cuenta_Destino'] = df_banregio['Cuenta_Destino'].astype(str)
+            st.write(df_banregio)
 
-        # Crear un enlace de descarga en Streamlit
-        #st.markdown(download_link(df_banregio, 'p_banregio.csv', 'Descargar Plantilla Banregio'), unsafe_allow_html=True)
+
+            def to_excel(df):
+                output = BytesIO()
+                with pd.ExcelWriter(output, engine='openpyxl') as writer:
+                    df.to_excel(writer, index=False, sheet_name='Sheet1')
+                processed_data = output.getvalue()
+                return processed_data
+            def get_download_link(data, filename, text):
+                b64 = base64.b64encode(data).decode()
+                return f'<a href="data:application/octet-stream;base64,{b64}" download="{filename}">{text}</a>'
+            excel_data = to_excel(df_banregio)
+            st.markdown(get_download_link(excel_data, 'p_banregio.xlsx', 'Descargar plantilla Banregio'), unsafe_allow_html=True)
+        except TypeError:
+            st.warning("Cargando base...")
+        except KeyError:
+            st.error("⚠️LA BASE CARGADA NO ES COMPATIBLE.⚠️")
+    
+    if st.checkbox('Plantilla Banco Base'):
+        try:
+            
+           
+            if len(df_temp) > 0:
+
+                cadena_lista = []
+                for index, row in df_temp.iterrows():
+                    cadena_lista.append("SP|"+ row['Proveedor'].replace(" ", "")+"|"+row['Clave'].replace(" ", "")+"|MXN|"+row['Id_banco'].replace(" ", "")+"|"+ row['Persona_tipo'].replace(" ", "")+"|||"+row['Proveedor'].replace(" ", "")+"|"+row['RFC'].replace(" ", "")+"||"+row['Correo'].replace(" ", "")+"||")
+                st.write(cadena_lista)
+
+                cadena_str2 = "\n".join(cadena_lista)
+                def get_text_download_link(text, filename, text_link):
+                    b64 = base64.b64encode(text.encode()).decode()
+                    return f'<a href="data:file/txt;base64,{b64}" download="{filename}">{text_link}</a>'
+                st.markdown(get_text_download_link(cadena_str2, 'Banco_Base.txt', 'Descargar Plantilla'), unsafe_allow_html=True)
+
+            else:
+                cadena = []
+                for index, row in st.session_state.filtered_data.iterrows():
+                    cadena.append("SP|"+ row['Proveedor'].replace(" ", "")+"|"+row['Clave'].replace(" ", "")+"|MXN|"+row['Id_banco'].replace(" ", "")+"|"+ row['Persona_tipo'].replace(" ", "")+"|||"+row['Proveedor'].replace(" ", "")+"|"+row['RFC'].replace(" ", "")+"||"+row['Correo'].replace(" ", "")+"||")
+                st.write(cadena)
+
+                cadena_str = "\n".join(cadena)
+                def get_text_download_link(text, filename, text_link):
+                    b64 = base64.b64encode(text.encode()).decode()
+                    return f'<a href="data:file/txt;base64,{b64}" download="{filename}">{text_link}</a>'
+                st.markdown(get_text_download_link(cadena_str, 'Banco_Base.txt', 'Descargar Plantilla'), unsafe_allow_html=True)
+        except KeyError:
+            st.error("⚠️LA BASE CARGADA NO ES COMPATIBLE.⚠️ ")
+        except TypeError:
+            st.warning("Cargando base...")
+            
+
 if __name__ == '__main__':
     main()
